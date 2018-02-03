d2216b2fa70e25f03eec20a2f048d8cde0980fc9
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-02-03 13:07:56 -0600

    Initialize repository with custom (unformatted) .css and .js files.

---
+++ custom/custom.js
@@ -0,0 +1,118 @@
+// leave at least 2 line with only a star on it below, or doc generation fails
+/**
+ *
+ *
+ * Placeholder for custom user javascript
+ * mainly to be overridden in profile/static/custom/custom.js
+ * This will always be an empty file in IPython
+ *
+ * User could add any javascript in the `profile/static/custom/custom.js` file.
+ * It will be executed by the ipython notebook at load time.
+ *
+ * Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
+ *
+ *
+ * The object available at load time depend on the version of IPython in use.
+ * there is no guaranties of API stability.
+ *
+ * The example below explain the principle, and might not be valid.
+ *
+ * Instances are created after the loading of this file and might need to be accessed using events:
+ *     define([
+ *        'base/js/namespace',
+ *        'base/js/events'
+ *     ], function(IPython, events) {
+ *         events.on("app_initialized.NotebookApp", function () {
+ *             IPython.keyboard_manager....
+ *         });
+ *     });
+ *
+ * __Example 1:__
+ *
+ * Create a custom button in toolbar that execute `%qtconsole` in kernel
+ * and hence open a qtconsole attached to the same kernel as the current notebook
+ *
+ *    define([
+ *        'base/js/namespace',
+ *        'base/js/events'
+ *    ], function(IPython, events) {
+ *        events.on('app_initialized.NotebookApp', function(){
+ *            IPython.toolbar.add_buttons_group([
+ *                {
+ *                    'label'   : 'run qtconsole',
+ *                    'icon'    : 'fa-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
+ *                    'callback': function () {
+ *                        IPython.notebook.kernel.execute('%qtconsole')
+ *                    }
+ *                }
+ *                // add more button here if needed.
+ *                ]);
+ *        });
+ *    });
+ *
+ * __Example 2:__
+ *
+ * At the completion of the dashboard loading, load an unofficial javascript extension
+ * that is installed in profile/static/custom/
+ *
+ *    define([
+ *        'base/js/events'
+ *    ], function(events) {
+ *        events.on('app_initialized.DashboardApp', function(){
+ *            require(['custom/unofficial_extension.js'])
+ *        });
+ *    });
+ *
+ * __Example 3:__
+ *
+ *  Use `jQuery.getScript(url [, success(script, textStatus, jqXHR)] );`
+ *  to load custom script into the notebook.
+ *
+ *    // to load the metadata ui extension example.
+ *    $.getScript('/static/notebook/js/celltoolbarpresets/example.js');
+ *    // or
+ *    // to load the metadata ui extension to control slideshow mode / reveal js for nbconvert
+ *    $.getScript('/static/notebook/js/celltoolbarpresets/slideshow.js');
+ *
+ *
+ * @module IPython
+ * @namespace IPython
+ * @class customjs
+ * @static
+ */
+
+
+
+/* Function to make the Input prompts a little better looking */
+function input_cell_format(value, lines_number) {
+	if (value === undefined || value === null) {
+		return '&nbsp;'
+	} else if (value === '*') {
+		return '&nbsp;'
+	} else {
+		return value + ' &nbsp;';
+	}
+}
+
+define(['base/js/namespace', 'base/js/events', 'notebook/js/codecell'], function (jupyter, events, cell) {
+
+	/* Force our inputs to use our formatting function */
+    cell.CodeCell.input_prompt_function = input_cell_format;
+    cell.CodeCell.input_prompt_continuation = input_cell_format;
+
+    /* Once the kernel is ready we can change the input prompts*/
+    events.on("kernel_ready.Kernel", function () {
+    	// Select all the input prompts 
+    	var prompts = $('div.prompt.input_prompt').toArray();
+
+    	// For each, extract the number and change the text
+    	prompts.forEach(function(d) {
+    		var prompt_text = d.innerText;
+    		var prompt_num = /\d/.test(prompt_text) ? prompt_text.match(/\d+/g)[0]: "";
+
+    		console.log(d);
+    		// Set the innerText
+    		d.innerHTML = '&nbsp;' + prompt_num + ' &nbsp;';
+    	})
+    });
+});
