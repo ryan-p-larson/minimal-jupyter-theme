8dfb64b94af0aaa38d611c81ed8b80e90ae1bf73
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-02-05 18:11:09 -0600

    Changed input prompt formatting function. Added documentation and comments.

--- custom/custom.js
+++ custom/custom.js
@@ -2,77 +2,9 @@
 /**
  *
  *
- * Placeholder for custom user javascript
- * mainly to be overridden in profile/static/custom/custom.js
- * This will always be an empty file in IPython
+ * Minimal Jupyter Theme
  *
- * User could add any javascript in the `profile/static/custom/custom.js` file.
- * It will be executed by the ipython notebook at load time.
- *
- * Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
- *
- *
- * The object available at load time depend on the version of IPython in use.
- * there is no guaranties of API stability.
- *
- * The example below explain the principle, and might not be valid.
- *
- * Instances are created after the loading of this file and might need to be accessed using events:
- *     define([
- *        'base/js/namespace',
- *        'base/js/events'
- *     ], function(IPython, events) {
- *         events.on("app_initialized.NotebookApp", function () {
- *             IPython.keyboard_manager....
- *         });
- *     });
- *
- * __Example 1:__
- *
- * Create a custom button in toolbar that execute `%qtconsole` in kernel
- * and hence open a qtconsole attached to the same kernel as the current notebook
- *
- *    define([
- *        'base/js/namespace',
- *        'base/js/events'
- *    ], function(IPython, events) {
- *        events.on('app_initialized.NotebookApp', function(){
- *            IPython.toolbar.add_buttons_group([
- *                {
- *                    'label'   : 'run qtconsole',
- *                    'icon'    : 'fa-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
- *                    'callback': function () {
- *                        IPython.notebook.kernel.execute('%qtconsole')
- *                    }
- *                }
- *                // add more button here if needed.
- *                ]);
- *        });
- *    });
- *
- * __Example 2:__
- *
- * At the completion of the dashboard loading, load an unofficial javascript extension
- * that is installed in profile/static/custom/
- *
- *    define([
- *        'base/js/events'
- *    ], function(events) {
- *        events.on('app_initialized.DashboardApp', function(){
- *            require(['custom/unofficial_extension.js'])
- *        });
- *    });
- *
- * __Example 3:__
- *
- *  Use `jQuery.getScript(url [, success(script, textStatus, jqXHR)] );`
- *  to load custom script into the notebook.
- *
- *    // to load the metadata ui extension example.
- *    $.getScript('/static/notebook/js/celltoolbarpresets/example.js');
- *    // or
- *    // to load the metadata ui extension to control slideshow mode / reveal js for nbconvert
- *    $.getScript('/static/notebook/js/celltoolbarpresets/slideshow.js');
+ * custom.js
  *
  *
  * @module IPython
@@ -82,18 +14,25 @@
  */
 
 
-
-/* Function to make the Input prompts a little better looking */
+/**
+  * @method
+  * @description Function to format an Jupyter/IPython cell prompt to cut down on cruft.
+  * @param {value} Int [1+] denoting the order in which the cell was fired.
+  * @param {lines_number} Int [0+] denoting how many lines of code cell has.
+  * @returns HTML formatted input prompt [<1 &nbsp;>, <&nbsp;>]
+  */
 function input_cell_format(value, lines_number) {
-	if (value === undefined || value === null) {
-		return '&nbsp;'
-	} else if (value === '*') {
-		return '&nbsp;'
-	} else {
-		return value + ' &nbsp;';
-	}
+    if (value === undefined || value === null) {
+        return '&nbsp;'
+    } else if (value === '*') {
+        return '&nbsp;'
+    } else {
+        return value + '.&nbsp;';
+    }
 }
 
+// On startup assign our custom function to Jupyter,
+// and parse the existing prompts to conform to our style.
 define(['base/js/namespace', 'base/js/events', 'notebook/js/codecell'], function (jupyter, events, cell) {
 
 	/* Force our inputs to use our formatting function */
@@ -102,15 +41,15 @@ define(['base/js/namespace', 'base/js/events', 'notebook/js/codecell'], function
 
     /* Once the kernel is ready we can change the input prompts*/
     events.on("kernel_ready.Kernel", function () {
+
     	// Select all the input prompts 
     	var prompts = $('div.prompt.input_prompt').toArray();
 
     	// For each, extract the number and change the text
     	prompts.forEach(function(d) {
     		var prompt_text = d.innerText;
-    		var prompt_num = /\d/.test(prompt_text) ? prompt_text.match(/\d+/g)[0]: "";
+    		var prompt_num = /\d/.test(prompt_text) ? prompt_text.match(/\d+/g)[0] + "." : "";
 
-    		console.log(d);
     		// Set the innerText
     		d.innerHTML = '&nbsp;' + prompt_num + ' &nbsp;';
     	})
